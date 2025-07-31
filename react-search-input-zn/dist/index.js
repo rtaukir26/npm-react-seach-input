20a8ebf13b66a0a834c83e7cc00f574145e86997
rtaukir26 <tauqueer.ansari@bluebinaries.com>
2025-07-31 10:14:07 +0530

    dist pushed

--- react-search-input-zn/dist/index.js
+++ react-search-input-zn/dist/index.js
@@ -13,23 +13,23 @@ var V;
 function ae() {
   if (V) return p;
   V = 1;
-  var u = Symbol.for("react.transitional.element"), f = Symbol.for("react.fragment");
-  function A(d, o, s) {
+  var u = Symbol.for("react.transitional.element"), A = Symbol.for("react.fragment");
+  function d(m, o, s) {
     var l = null;
     if (s !== void 0 && (l = "" + s), o.key !== void 0 && (l = "" + o.key), "key" in o) {
       s = {};
-      for (var m in o)
-        m !== "key" && (s[m] = o[m]);
+      for (var i in o)
+        i !== "key" && (s[i] = o[i]);
     } else s = o;
     return o = s.ref, {
       $$typeof: u,
-      type: d,
+      type: m,
       key: l,
       ref: o !== void 0 ? o : null,
       props: s
     };
   }
-  return p.Fragment = f, p.jsx = A, p.jsxs = A, p;
+  return p.Fragment = A, p.jsx = d, p.jsxs = d, p;
 }
 var v = {};
 /**
@@ -87,12 +87,12 @@ function oe() {
         }
       return null;
     }
-    function f(e) {
+    function A(e) {
       return "" + e;
     }
-    function A(e) {
+    function d(e) {
       try {
-        f(e);
+        A(e);
         var r = !1;
       } catch {
         r = !0;
@@ -104,10 +104,10 @@ function oe() {
           r,
           "The provided key is an unsupported type %s. This value must be coerced to a string before using it here.",
           n
-        ), f(e);
+        ), A(e);
       }
     }
-    function d(e) {
+    function m(e) {
       if (e === O) return "<>";
       if (typeof e == "object" && e !== null && e.$$typeof === U)
         return "<...>";
@@ -132,7 +132,7 @@ function oe() {
       }
       return e.key !== void 0;
     }
-    function m(e, r) {
+    function i(e, r) {
       function t() {
         I || (I = !0, console.error(
           "%s: `key` is not a prop. Trying to access it will result in `undefined` being returned. If you need to access the same value within the child component, you should pass it as a different prop. (https://react.dev/link/special-props)",
@@ -150,13 +150,13 @@ function oe() {
         "Accessing element.ref was removed in React 19. ref is now a regular prop. It will be removed from the JSX Element type in a future release."
       )), e = this.props.ref, e !== void 0 ? e : null;
     }
-    function S(e, r, t, n, i, c, P, j) {
+    function S(e, r, t, n, f, c, C, j) {
       return t = c.ref, e = {
         $$typeof: g,
         type: e,
         key: r,
         props: c,
-        _owner: i
+        _owner: f
       }, (t !== void 0 ? t : null) !== null ? Object.defineProperty(e, "ref", {
         enumerable: !1,
         get: h
@@ -174,7 +174,7 @@ function oe() {
         configurable: !1,
         enumerable: !1,
         writable: !0,
-        value: P
+        value: C
       }), Object.defineProperty(e, "_debugTask", {
         configurable: !1,
         enumerable: !1,
@@ -182,25 +182,25 @@ function oe() {
         value: j
       }), Object.freeze && (Object.freeze(e.props), Object.freeze(e)), e;
     }
-    function R(e, r, t, n, i, c, P, j) {
+    function _(e, r, t, n, f, c, C, j) {
       var a = r.children;
       if (a !== void 0)
         if (n)
           if (ee(a)) {
             for (n = 0; n < a.length; n++)
-              _(a[n]);
+              b(a[n]);
             Object.freeze && Object.freeze(a);
           } else
             console.error(
               "React.jsx: Static children should always be an array. You are likely explicitly calling React.jsxs or React.jsxDEV. Use the Babel transform instead."
             );
-        else _(a);
+        else b(a);
       if (y.call(r, "key")) {
         a = u(e);
-        var b = Object.keys(r).filter(function(re) {
+        var R = Object.keys(r).filter(function(re) {
           return re !== "key";
         });
-        n = 0 < b.length ? "{key: someKey, " + b.join(": ..., ") + ": ...}" : "{key: someKey}", L[a + n] || (b = 0 < b.length ? "{" + b.join(": ..., ") + ": ...}" : "{}", console.error(
+        n = 0 < R.length ? "{key: someKey, " + R.join(": ..., ") + ": ...}" : "{key: someKey}", L[a + n] || (R = 0 < R.length ? "{" + R.join(": ..., ") + ": ...}" : "{}", console.error(
           `A props object containing a "key" prop is being spread into JSX:
   let props = %s;
   <%s {...props} />
@@ -209,33 +209,33 @@ React keys must be passed directly to JSX without using spread:
   <%s key={someKey} {...props} />`,
           n,
           a,
-          b,
+          R,
           a
         ), L[a + n] = !0);
       }
-      if (a = null, t !== void 0 && (A(t), a = "" + t), l(r) && (A(r.key), a = "" + r.key), "key" in r) {
+      if (a = null, t !== void 0 && (d(t), a = "" + t), l(r) && (d(r.key), a = "" + r.key), "key" in r) {
         t = {};
         for (var N in r)
           N !== "key" && (t[N] = r[N]);
       } else t = r;
-      return a && m(
+      return a && i(
         t,
         typeof e == "function" ? e.displayName || e.name || "Unknown" : e
       ), S(
         e,
         a,
         c,
-        i,
+        f,
         o(),
         t,
-        P,
+        C,
         j
       );
     }
-    function _(e) {
+    function b(e) {
       typeof e == "object" && e !== null && e.$$typeof === g && e._store && (e._store.validated = 1);
     }
-    var E = te, g = Symbol.for("react.transitional.element"), w = Symbol.for("react.portal"), O = Symbol.for("react.fragment"), z = Symbol.for("react.strict_mode"), q = Symbol.for("react.profiler"), J = Symbol.for("react.consumer"), X = Symbol.for("react.context"), Q = Symbol.for("react.forward_ref"), M = Symbol.for("react.suspense"), Z = Symbol.for("react.suspense_list"), K = Symbol.for("react.memo"), U = Symbol.for("react.lazy"), $ = Symbol.for("react.activity"), H = Symbol.for("react.client.reference"), x = E.__CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE, y = Object.prototype.hasOwnProperty, ee = Array.isArray, C = console.createTask ? console.createTask : function() {
+    var E = te, g = Symbol.for("react.transitional.element"), w = Symbol.for("react.portal"), O = Symbol.for("react.fragment"), z = Symbol.for("react.strict_mode"), q = Symbol.for("react.profiler"), J = Symbol.for("react.consumer"), X = Symbol.for("react.context"), Q = Symbol.for("react.forward_ref"), M = Symbol.for("react.suspense"), Z = Symbol.for("react.suspense_list"), K = Symbol.for("react.memo"), U = Symbol.for("react.lazy"), $ = Symbol.for("react.activity"), H = Symbol.for("react.client.reference"), x = E.__CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE, y = Object.prototype.hasOwnProperty, ee = Array.isArray, P = console.createTask ? console.createTask : function() {
       return null;
     };
     E = {
@@ -246,30 +246,30 @@ React keys must be passed directly to JSX without using spread:
     var I, Y = {}, F = E.react_stack_bottom_frame.bind(
       E,
       s
-    )(), B = C(d(s)), L = {};
-    v.Fragment = O, v.jsx = function(e, r, t, n, i) {
+    )(), B = P(m(s)), L = {};
+    v.Fragment = O, v.jsx = function(e, r, t, n, f) {
       var c = 1e4 > x.recentlyCreatedOwnerStacks++;
-      return R(
+      return _(
         e,
         r,
         t,
         !1,
         n,
-        i,
+        f,
         c ? Error("react-stack-top-frame") : F,
-        c ? C(d(e)) : B
+        c ? P(m(e)) : B
       );
-    }, v.jsxs = function(e, r, t, n, i) {
+    }, v.jsxs = function(e, r, t, n, f) {
       var c = 1e4 > x.recentlyCreatedOwnerStacks++;
-      return R(
+      return _(
         e,
         r,
         t,
         !0,
         n,
-        i,
+        f,
         c ? Error("react-stack-top-frame") : F,
-        c ? C(d(e)) : B
+        c ? P(m(e)) : B
       );
     };
   }()), v;
@@ -281,43 +281,43 @@ function se() {
 var T = se();
 const ce = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAACxAAAAsQHGLUmNAAAAGXRFWHRTb2Z0d2FyZQB3d3cuaW5rc2NhcGUub3Jnm+48GgAAAS1JREFUSInt1D1KQ0EUhuFHBQVJtHQBdpogWopFsEqhO9CNCLoEUSK6B8UFiFHcgUXiBhT8w0ptrGIxE5BLcp2bkEb8YGB+zpx3mDnf8K8hVcUh2viMrY0DVFISjPeZn8IxLvCKLczFto03XOIIk0VPPYVrnKKcE1fGGa6KQk5i8rGE2LEIaaQmr+JR/smzmsETFnstTmTGO7hBswDgC9NYFd4sV3dYKpC8q2W0UgI/UBoAUMZ7r4VsmXakPW5WffdkAQ+YHwAwj/sUwCU2BgBsCn74VRWh5IqU6SyesZC64UgwT6rRzhUwGsH2zQiZyYmbjck7qBcBdCEN4bp2sSKUbyn29/ASY+qxXysKIdh/XzDRh/Bdt4Qv/Oed14aBpGotQtZHCanhdpSAP6hv1hk2Sb7pAkUAAAAASUVORK5CYII=", ue = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAAsQAAALEBxi1JjQAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAACbSURBVEiJ7dIxDkFBFIXhL9EjFqFQEjag0WgtQ63RquxCaSUklqAUlqCjeKbBPMkYiWL+cu7NOZNzLoXC39Oomc3RwrFmZ4krTinmI1wwiczX2D8+kUwfZ0yf3lc4oPONeMwkq3ggxLVVxdLOKR7Y4IbZL8RDLGPvO8kiHjKPFZ9FPJDFZIGd+J0PVcUPUg16aH7Y6cp8roXCK3cSgxwkA5AYlwAAAABJRU5ErkJggg==", ie = ({
   data: u = [],
-  handleChange: f,
-  searchLabel: A = "Search",
-  styles: d
+  handleChange: A,
+  searchLabel: d = "Search",
+  matchesWith: m,
+  styles: o
 }) => {
-  const [o, s] = D(""), [l, m] = D([]);
+  const [s, l] = D(""), [i, h] = D([]);
   ne(() => {
-    m(u);
+    h(u);
   }, []);
-  const h = (S) => {
-    let { value: R } = S.target;
-    s(R);
-    let _ = l?.filter((E) => {
-      const g = E?.name?.toLowerCase().includes(R?.toLowerCase()), w = E?.value?.toLowerCase()?.includes(R?.toLowerCase());
-      return g || w;
-    });
-    f(_);
+  const S = (_) => {
+    let { value: b } = _.target;
+    l(b);
+    const E = i?.filter((g) => m?.some(
+      (w) => g[w]?.toLowerCase().includes(b?.toLowerCase())
+    ));
+    A(E);
   };
   return /* @__PURE__ */ T.jsxs("div", { className: "search-container", children: [
     /* @__PURE__ */ T.jsx(
       "input",
       {
         className: "rsi-input",
-        style: d,
+        style: o,
         type: "text",
-        placeholder: A,
-        value: o,
-        onChange: h
+        placeholder: d,
+        value: s,
+        onChange: S
       }
     ),
-    o && /* @__PURE__ */ T.jsx(
+    s && /* @__PURE__ */ T.jsx(
       "img",
       {
         className: "rsi-close zoom-anim",
         src: ue,
         alt: "close",
         onClick: () => {
-          s(""), f(l);
+          l(""), A(i);
         }
       }
     ),
